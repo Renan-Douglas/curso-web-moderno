541f803dbeff3738def1535011195f6b8bbbab43
Renan Douglas Ferreira Bimbato <113561549+Renan-Douglas@users.noreply.github.com>
2023-01-03 19:53:41 -0300

    desafios

---
+++ inicio/desafio-area.js
@@ -0,0 +1,7 @@
+const PI = 3        //    VALOR EXATO --> 3.14159265359
+
+let raio = 5
+
+let resArea = PI * raio * raio
+
+console.log(resArea)
---
+++ inicio/desafio-troca.js
@@ -0,0 +1,9 @@
+let a = 3;
+let b = 34;
+
+let alt = a
+a = b
+b = alt
+
+console.log(a)
+console.log(b)

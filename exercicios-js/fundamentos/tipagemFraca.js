aaa78a1b5ae61a99ac02014363e114620b9dc879
Renan Douglas Ferreira Bimbato <113561549+Renan-Douglas@users.noreply.github.com>
2023-01-15 18:49:31 -0300

    Tipagem

---
+++ exercicios-js/fundamentos/tipagemFraca.js
@@ -0,0 +1,14 @@
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.45123
+console.log(qualquer)
+console.log(typeof qualquer)
+console.log(typeof qualquer)
+
+// Evitar nome generico e siglas
+
+let valor = ''
+let numero = 2
+let pqp = false // Produto Quimico Perigoso... kkk

44ef97fb573138407dd9de2a2c43fc4640ef8c4d
Renan Bimbato <113561549+Renan-Douglas@users.noreply.github.com>
2023-04-07 19:43:14 -0300

    callback

---
+++ exercicios-js/funcao/callback1.js
@@ -0,0 +1,8 @@
+const fabricantes = ['Mercedes', 'Audi', 'BMW', 'Ford', 'Volvo']
+
+function imprimir(nome, indice) {
+    console.log(`${indice + 1}. ${nome}`)
+}
+
+fabricantes.forEach(imprimir)
+fabricantes.forEach(fabricante => console.log(fabricante))
